160a56f85be7e78a5f77fec24195d3669b960cd9
SanyamSamS <sanyam.edu@gmail.com>
2024-02-28 22:42:51 -0500

    added routes for thoughts and users

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { 
+    getThoughts, 
+    getThoughtById, 
+    createThought, 
+    updateThought, 
+    deleteThought, 
+    addReaction, 
+    deleteReaction 
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/')
+    .get(getThoughts)
+    .post(createThought);
+
+router.route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router.route('/:thoughtId/reactions')  
+    .post(addReaction)
+    .delete(deleteReaction);
+
+module.exports = router;
