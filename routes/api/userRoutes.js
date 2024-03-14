171e55408d3caec6271abdb21a3426fbb23a187c
SanyamSamS <sanyam.edu@gmail.com>
2024-03-14 17:34:52 -0400

    fixed id routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -14,15 +14,13 @@ router.route('/')
     .get(getUsers)
     .post(createUser);
 
-router.route('/:id')
+router.route('/:userId')
     .get(getUserById)
     .put(updateUser)
     .delete(deleteUser);
 
-router.route('/:userId/friends/')
-    .post(addFriend)
-
 router.route('/:userId/friends/:friendId')
-    .delete(deleteFriend);
+    .delete(deleteFriend)
+    .post(addFriend);
 
 module.exports = router;
